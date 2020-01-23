52bed4a3a9196077d9dae86d66705a25e19a41d0
axaidan <axaidan@hotmail.com>
2020-01-23 17:58:35 +0100

    rspec init + modif maire_spec

---
+++ spec/mairie_christmas_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/mairie_christmas'
+
+=begin
+describe "groupe de tests" do
+	it "test d'une fonction" do
+		expect(fonction(argument)).to eq(return)
+	end
+end
+=end
+
+
+describe "groupe de tests" do
+	it "test d'une fonction" do
+		expect(fonction(argument)).to eq("95/ableiges.html")
+	end
+end
